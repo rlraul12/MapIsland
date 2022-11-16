7f98e0b86c39e55b56f9d2a5cf54cd57ed4d5028
Raul E. Paneque <49737084+rlraul12@users.noreply.github.com>
2022-11-16 16:08:36 -0500

    Initial commit

---
+++ Landmasck/LandmasckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmasckApp.swift
+//  Landmasck
+//
+//  Created by Raul Paneque on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmasckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
